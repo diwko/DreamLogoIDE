9c868e17b099d82da2a95d166e00af9c00a0dcb7
Dawid Siwko <dawid.siwko@gmail.com>
2017-12-04 17:18:46 +0100

    remove drawing responsibility, add new properties

--- src/main/java/model/Turtle.java
+++ src/main/java/model/Turtle.java
@@ -1,68 +1,94 @@
 package model;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.paint.Color;
 
 public class Turtle {
-    private Position position;
-    private boolean isPenDown = true;
-    private Drawer drawer;
+    private ObjectProperty<Position> position = new SimpleObjectProperty<>();
+    private Area area;
 
-    public Turtle(Position position, Drawer drawer) {
-        this.position = position;
-        this.drawer = drawer;
-    }
+    private boolean hidden = false;
+    private boolean penDown = true;
+    private DoubleProperty penSize = new SimpleDoubleProperty(1.0);
+    private ObjectProperty<Color> penColor = new SimpleObjectProperty<>(Color.BLACK);
 
-    public Turtle(Position position, boolean isPenDown, Drawer drawer) {
-        this.position = position;
-        this.isPenDown = isPenDown;
-        this.drawer = drawer;
+    public Turtle(Position position, Area area) {
+        setPosition(position);
+        this.area = area;
     }
 
     public Position getPosition() {
-        return position;
+        return position.getValue();
     }
 
     public void setPosition(Position position) {
-        this.position = position;
+        this.position.setValue(position);
+    }
+
+    public ObjectProperty<Position> positionProperty() {
+        return position;
+    }
+
+    public Area getArea() {
+        return area;
+    }
+
+    public void setArea(Area area) {
+        this.area = area;
+    }
+
+    public boolean isHidden() {
+        return hidden;
+    }
+
+    public void setHidden(boolean hidden) {
+        this.hidden = hidden;
     }
 
     public boolean isPenDown() {
-        return isPenDown;
+        return penDown;
     }
 
-    public Drawer getDrawer() {
-        return drawer;
+    public void setPenDown(boolean penDown) {
+        this.penDown = penDown;
     }
 
-    public void setDrawer(Drawer drawer) {
-        this.drawer = drawer;
+    public Color getPenColor() {
+        return penColor.getValue();
+    }
+
+    public void setPenColor(Color penColor) {
+        this.penColor.setValue(penColor);
+    }
+
+    public ObjectProperty<Color> penColorProperty() {
+        return penColor;
+    }
+
+    public double getPenSize() {
+        return penSize.getValue();
+    }
+
+    public void setPenSize(double penSize) {
+        this.penSize.setValue(penSize);
+    }
+
+    public DoubleProperty penSizeProperty() {
+        return penSize;
     }
 
     public void move(double distance) throws IllegalStateException {
-        Position newPosition = position.addDistance(distance);
-        if (!drawer.isCorrectPosition(newPosition))
+        Position newPosition = getPosition().addDistance(distance);
+        if (!area.isCorrectPosition(newPosition))
             throw new IllegalStateException("New position is outside the area");
-
-        if (isPenDown)
-            drawer.drawLine(position, newPosition);
-
-        position = newPosition;
+        setPosition(newPosition);
     }
 
     public void rotate(double angle) {
-        position = position.addRotation(angle);
-    }
-
-    public void penDown() {
-        isPenDown = true;
-    }
-
-    public void penUp() {
-        isPenDown = false;
-    }
-
-    public void setPenColor(Color color) {
-        drawer.changeColor(color);
+        setPosition(getPosition().addRotation(angle));
     }
 
 
